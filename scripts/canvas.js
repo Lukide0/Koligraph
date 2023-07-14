d6686803da0aa5667a2a43c8fc04a09a686ee370
Lukáš Koliandr <koliandr.lu@gmail.com>
2023-07-14 16:54:51 +0200

    feat: create and move nodes

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,26 +1,137 @@
+const SHAPES = {
+  BOX: "BOX",
+  BOX_ROUND_EDGES: "BOX_ROUND_EDGES",
+  STADIUM: "STADIUM",
+  DATABASE: "DATABASE",
+  CIRCLE: "CIRCLE",
+  DOUBLE_CIRCLE: "DOUBLE_CIRCLE",
+  FLAG: "FLAG",
+  RHOMBUS: "RHOMBUS",
+  HEXAGON: "HEXAGON",
+  PARALLELOGRAM: "PARALLELOGRAM",
+  PARALLELOGRAM_ALT: "PARALLELOGRAM_ALT",
+  TRAPEZOID: "TRAPEZOID",
+  TRAPEZOID_ALT: "TRAPEZOID_ALT",
+};
+
 class Canvas {
-    constructor(element) 
-    {
-        this.element = element;
-        this.viewPos = { x: 0, y: 0 };
-        this.shapes = [];
+  constructor(element) {
+    this.element = element;
+    this.boundRect = element.getBoundingClientRect();
+    this.viewPos = { x: 0, y: 0 };
+    this.shapes = [];
+    this._moveEl = null;
+    this._mouseStart = { x: 0, y: 0 };
 
-        let self = this;
+    this._initMove();
+  }
 
-        this.element.onclick = function(e) {
-            Log.info('click');
-            self.createShape(e.offsetX + self.viewPos.x, e.offsetY + self.viewPos.y);
-        }
+  _initMove() {
+    let that = this;
+
+    function moveHelper(e) {
+      that._moveEvent(e, that._moveEl.shape, that._mouseStart);
     }
 
-    createShape(x, y) 
-    {
-        let el = document.createElement('div');
-        el.style = "width:10px; height: 10px; background-color: red; position: absolute";
-        el.style.top = `${y}px`;
-        el.style.left = `${x}px`;
-    
-        this.element.appendChild(el);
+    this.element.onmousedown = function (e) {
+      that._moveEl = e.target.closest(".shape");
+
+      if (that._moveEl !== null) {
+        that._mouseStart = {
+          x: e.clientX - that._moveEl.shape.pos.x,
+          y: e.clientY - that._moveEl.shape.pos.y,
+        };
+
+        that._moveEl.style.userSelect = "none";
+        that.element.addEventListener("mousemove", moveHelper);
+      }
+    };
+
+    this.element.onmouseup = (e) => {
+      if (that._moveEl !== null) {
+        that.element.removeEventListener("mousemove", moveHelper);
+        that._moveEl.style.userSelect = "auto";
+        that._moveEl = null;
+      }
+    };
+  }
+
+  addShape(shape) {
+    this.shapes.push(shape);
+    this.element.appendChild(shape.element);
+  }
+
+  _moveEvent(e, shape, mouseStart) {
+    let x = e.clientX - mouseStart.x;
+    let y = e.clientY - mouseStart.y;
+
+    shape.setPos(Math.max(x, 0), Math.max(y, 0));
+  }
+}
+
+class Shape {
+  constructor(x, y, shape = SHAPES.BOX) {
+    this.pos = { x, y };
+    this.shape = shape;
+
+    this.element = document.createElement("div");
+    this.element.classList.add("shape");
+    this.element.shape = this;
+
+    this.textElement = document.createElement("p");
+    this.element.appendChild(this.textElement);
+
+    this.setPos(x, y);
+
+    let that = this;
+
+    this.element.ondblclick = (e) => {
+      that.textElement.contentEditable = true;
+      that.textElement.focus();
+
+      e.stopPropagation();
+      e.preventDefault();
+    };
+
+    this.textElement.onblur = () => {
+      that.textElement.contentEditable = false;
+    };
+
+    switch (shape) {
+      case SHAPES.BOX_ROUND_EDGES:
+        this.element.classList.add("round");
+        break;
+      case SHAPES.STADIUM:
+        this.element.classList.add("statium");
+        break;
+      case SHAPES.CIRCLE:
+      case SHAPES.DATABASE:
+      case SHAPES.DOUBLE_CIRCLE:
+      case SHAPES.FLAG:
+      case SHAPES.RHOMBUS:
+      case SHAPES.HEXAGON:
+      case SHAPES.PARALLELOGRAM:
+      case SHAPES.PARALLELOGRAM_ALT:
+      case SHAPES.TRAPEZOID:
+      case SHAPES.TRAPEZOID_ALT:
+      default:
+        break;
     }
-    
+  }
+
+  inView(startX, startY, endX, endY) {
+    return (
+      this.pos.x >= startX &&
+      this.pos.x < endX &&
+      this.pos.y >= startY &&
+      this.pos.y < this.pos.y
+    );
+  }
+
+  setPos(x, y) {
+    this.element.style.left = `${x}px`;
+    this.element.style.top = `${y}px`;
+    this.pos.x = x;
+    this.pos.y = y;
+  }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,43 @@
 const Log = {
+  _currTimeAsStr() {
+    let d = new Date().toISOString();
 
-    _currTimeAsStr() {
-        let d = new Date().toISOString();
+    return d
+      .substring(0, d.length - 1)
+      .split("T")
+      .join(" ");
+  },
 
-        return d.substring(0, d.length - 1).split('T').join(' ');
-    },
-
-    success(msg) {
-        console.log(`%cSUCCESS (${this._currTimeAsStr()}):`, 'color: #1faa00', msg);
-    },
-    info(msg) {
-        console.info(`%cINFO (${this._currTimeAsStr()}):`, 'color: #1976D2', msg);
-    },
-    warn(msg) {
-        console.warn(`%cWARNING (${this._currTimeAsStr()}):`, 'color: #FFA000', msg);
-    },
-    err(msg) {
-        console.error(`%cERROR (${this._currTimeAsStr()}):`, 'color: #D32F2F', msg);
-    }
-}
+  success(msg) {
+    console.log(`%cSUCCESS (${this._currTimeAsStr()}):`, "color: #1faa00", msg);
+  },
+  info(msg) {
+    console.info(`%cINFO (${this._currTimeAsStr()}):`, "color: #1976D2", msg);
+  },
+  warn(msg) {
+    console.warn(
+      `%cWARNING (${this._currTimeAsStr()}):`,
+      "color: #FFA000",
+      msg
+    );
+  },
+  err(msg) {
+    console.error(`%cERROR (${this._currTimeAsStr()}):`, "color: #D32F2F", msg);
+  },
+};
 
 function fatalErr(msg) {
-    Log.err(msg);
-    throw new Error("Fatal error");
+  Log.err(msg);
+  throw new Error("Fatal error");
 }
 
 const previewEl = document.getElementById("preview");
 const canvas = new Canvas(document.getElementById("canvas"));
 
+canvas.element.ondblclick = function (e) {
+  let x = e.clientX - canvas.boundRect.left;
+  let y = e.clientY - canvas.boundRect.top;
+  let shape = new Shape(x, y);
 
-
-
-
-
-
-
+  canvas.addShape(shape);
+};
